f82bc442480c4bdf5c8e2ad804bc9ff37dcdbc9a
Priyam Suthar <suthar.p@husky.neu.edu>
2020-11-08 18:54:58 -0500

    Merge conflicts

--- src/api.js
+++ src/api.js
@@ -1,2 +1,2 @@
 export const REQUEST_API_LOCAL = 'http://localhost:5000/'
-export const REQUEST_API_HEROKU = 'http://expert-hiring-node.herokuapp.com/'
+export const REQUEST_API_HEROKU = 'https://expert-hiring-node.herokuapp.com/'
